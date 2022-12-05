89f4c8799d7c62e0e50d87506d73790333b07150
Danish-Arshad <Danisharshad976@gmail.com>
2022-12-05 10:20:55 +0500

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrandsComponentComponent } from './brands-component/brands-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:BrandsComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
