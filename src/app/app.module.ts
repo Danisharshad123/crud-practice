4e7e6f8abb70de1b54e537589474b459a6383902
Danish-Arshad <Danisharshad976@gmail.com>
2022-12-09 11:37:33 +0500

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
 import { BrandsComponentComponent } from './brands-component/brands-component.component';
 
 @NgModule({
@@ -12,6 +14,8 @@ import { BrandsComponentComponent } from './brands-component/brands-component.co
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
     
   ],
   providers: [],
--- src/app/brands-component/brands-component.component.ts
+++ src/app/brands-component/brands-component.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import { brandmodel } from './brand.module';
-
+import { FormBuilder,FormGroup, FormControl } from "@angular/forms";
 @Component({
   selector: 'app-brands-component',
   templateUrl: './brands-component.component.html',
   styleUrls: ['./brands-component.component.css']
 })
 export class BrandsComponentComponent {
-  
 
+    Products:FormGroup = new FormGroup({
+      mobilename : new FormControl(),
+      mobilemodel : new FormControl(),
+      mobilemake : new FormControl(),
+      mobileprice : new FormControl(),
+    })
   
   constructor(){}
 
