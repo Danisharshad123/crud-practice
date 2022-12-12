02e985ce2074febbec0b121033ca75ac5baadbee
Danish-Arshad <Danisharshad976@gmail.com>
2022-12-12 11:27:13 +0500

    add

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Data } from '@angular/router';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +9,42 @@ import { Injectable } from '@angular/core';
 export class ApiService {
 
   constructor(private Api:HttpClient) { }
+  
+  Postdata(data:any){
+    return this.Api.post<any>('http://localhost:3000/Posts/',data).pipe(
+      map(
+        (res =>
+          {
+            return res
+          })
+      ))}
+
+    Getdata(){
+        return this.Api.get<any>('http://localhost:3000/Posts/').pipe(
+          map(
+            (res =>
+              {
+                return res
+              })
+          ))}
+          Putdata(data:any, id:number){
+            return this.Api.put<any>('http://localhost:3000/Posts/'+id ,data).pipe(
+              map(
+                (res =>
+                  {
+                    return res
+                  })
+              ))}
+            Deletedata(id:number){
+                return this.Api.delete<any>('http://localhost:3000/Posts/'+id).pipe(
+                  map(
+                    (res =>
+                      {
+                        return res
+                      })
+                  ))}
+    
+
+
 
 }
